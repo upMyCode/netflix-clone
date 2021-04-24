7600dcbafef3e8463420c4ffdbadda0fd0614caf
Ulad Birukou <alorine99@gmail.com>
2021-04-24 22:08:11 +0300

    netflix-clone-day-1

--- src/Banner.js
+++ src/Banner.js
@@ -1,23 +1,51 @@
-import React from 'react'
+import React,{useEffect, useState} from 'react'
 import './Banner.css'
+import axios from './axios'
+import requests from './Request'
 
 function Banner() {
+    const [movie, setMovie] = useState([])
+
+    useEffect(() => {
+        async function fetchData() {
+            const request = await axios.get(requests.fetchNetflixOriginals)
+            setMovie(
+                request.data.results[
+                    Math.floor(Math.random() * request.data.results.length - 1)
+                ]
+            );
+            return request;
+        }
+
+        fetchData()
+    }, [])
+
+    function truncate(string, n) {
+        return string?.length > n ? string.substr(0, n - 1) + '...' : string;
+    }
+
     return (
-        <div 
+        <header 
             className='banner' 
             style={{
-                backgroundImage: `url('https://asicsulb.org/corporate/images/connect/beachfront/2020-06/awaken-your-creativity-with-netflix-inspired-meals.jpg')`,
+                backgroundImage: `url('https://image.tmdb.org/t/p/original/${movie?.backdrop_path}')`,
                 backgroundSize: 'cover',
                 backgroundPosition: 'center center'
         }}>
-            <div className='banner-contents'>
-                <h1 className='banner__title'>Movie Name</h1>
+            <div className='banner__contents'>
+                <h1 className='banner__title'>
+                    {movie?.title || movie?.name || movie?.original_name}
+                </h1>
                 <div className='banner__buttons'>
-                    <button className='banner__buttons'>Play</button>
-                    <button className='banner__buttons'>My List</button>
+                    <button className='banner__button'>Play</button>
+                    <button className='banner__button'>My List</button>
                 </div>
+                <h1 className='banner__description'>
+                    {truncate(movie?.overview, 150)}
+                </h1>
             </div>
-        </div>
+            <div className='banner--fadeBottom'/>
+        </header>
     )
 }
 
